c0dbffcfed643c322a2c3da396abf54c0a30133d
Mezan2002 <mezanurr009@gmail.com>
2023-05-04 00:53:09 +0600

    Some bug of nav fixed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,30 +10,45 @@ const Nav = () => {
         {/* nav inner  */}
         <div className="w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
           <Link
+            activeClass="active"
+            spy={true}
+            smooth={true}
             to="home"
             className="cursor-pointer flex items-center justify-center w-[60px] h-[60px]"
           >
             <BiHomeAlt />
           </Link>
           <Link
+            activeClass="active"
+            spy={true}
+            smooth={true}
             to="about"
             className="cursor-pointer flex items-center justify-center w-[60px] h-[60px]"
           >
             <BiUser />
           </Link>
           <Link
+            activeClass="active"
+            spy={true}
+            smooth={true}
             to="service"
             className="cursor-pointer flex items-center justify-center w-[60px] h-[60px]"
           >
             <BsClipboardData />
           </Link>
           <Link
+            activeClass="active"
+            spy={true}
+            smooth={true}
             to="work"
             className="cursor-pointer flex items-center justify-center w-[60px] h-[60px]"
           >
             <BsBriefcase />
           </Link>
           <Link
+            activeClass="active"
+            spy={true}
+            smooth={true}
             to="contact"
             className="cursor-pointer flex items-center justify-center w-[60px] h-[60px]"
           >
